b55f547e365effa883eb8527de355f59ebe0d99e
jipdesign-jp <jipdesigninfo@gmail.com>
2022-05-30 16:14:54 +0900

    spell miss fixed

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    publisded_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.publisded_date = timezone.now()

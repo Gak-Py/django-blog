286675a7f6325f76b30a987b5ce3f8f51871d9cf
jipdesign-jp <jipdesigninfo@gmail.com>
2022-06-01 17:17:59 +0900

    dotenv added

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,10 +23,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b(b)2aarnd!jzz7+e-17plv#n6popy%kasoer0xj@0kwv4f4kb'
+try:
+    from .local_settings import *
+except ImportError:
+    import os
+    from dotenv import load_dotenv
+    load_dotenv(verbose=True)
+    project_folder = os.path.expanduser('~/jipdesign.pythonanywhere.com/')  # adjust as appropriate
+    load_dotenv(os.path.join(project_folder, '.env'))
+    SECRET_KEY = 'django-insecure-b(b)2aarnd!jzz7+e-17plv#n6popy%kasoer0xj@0kwv4f4kb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+    DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
